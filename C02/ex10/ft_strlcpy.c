72b55617ea8255c0ad9e1f0f986b1c810b29cee1
felixmustin <60741148+felixmustin@users.noreply.github.com>
2021-10-19 21:13:04 +0200

    C02

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmustin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/07 16:48:53 by fmustin           #+#    #+#             */
+/*   Updated: 2021/08/12 12:31:32 by fmustin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i] != '\0')
+		{
+			i++;
+		}
+		return (i);
+	}
+	while (src[i] != '\0' && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+	{
+		dest[i] = '\0';
+	}
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmustin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/07 17:31:40 by fmustin           #+#    #+#             */
+/*   Updated: 2021/08/12 12:33:25 by fmustin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_hexa(char nb)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	ft_putchar(hex[nb / 16]);
+	ft_putchar(hex[nb % 16]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 0 && str[i] < 32 && str[i] > 126)
+		{
+			ft_putchar('\\');
+			ft_print_hexa(str[i]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
+}
